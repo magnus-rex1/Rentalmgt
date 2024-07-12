817e989b17715df60b1e6daca0eec2c38ebf51fd
magnus-rex1 <barrykawooya@gmail.com>
2024-07-12 20:50:45 +0300

    property input

--- resources/js/Pages/Rentals/Index.jsx
+++ resources/js/Pages/Rentals/Index.jsx
@@ -26,23 +26,35 @@ export default function index({ auth }) {
             <Head title="Properties" />
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
-                    <label for="address">Address: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="address" name="address" placeholder="Enter property address" />
-                    <br />
-                    <label for="type">Type: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="type" name="type" placeholder="Enter property type" />
-                    <br />
-                    <label for="size">Size: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="size" name="size" placeholder="Enter property size" />
-                    <br />
-                    <label for="bedrooms">Bedrooms: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="bedrooms" name="bedrooms" placeholder="Enter number of bedrooms" />
-                    <br />
-                    <label for="rent">Rent: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="rent" name="rent" placeholder="Enter rent amount" />
-                    <br />
-                    <label for="available">Available: </label>
-                    <input className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="available" name="available" placeholder="(yes/no)" />
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="address" name="address" placeholder="Enter property address" />
+                        <label for="address">Address: </label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="type" name="type" placeholder="Enter property type" />
+                        <label for="type">Type: </label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="size" name="size" placeholder="Enter property size" />
+                        <label for="size">Size: </label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="bedrooms" name="bedrooms" placeholder="Enter number of bedrooms" />
+                        <label for="bedrooms">Bedrooms: </label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="rent" name="rent" placeholder="Enter rent amount" />
+                        <label for="rent">Rent: </label>
+                    </div>
+
+                    <div className="form-floating mb-3">
+                        <input className="form-control block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text" id="available" name="available" placeholder="(yes/no)" />
+                        <label for="available">Available: </label>
+                    </div>
 
                     <InputError message={errors.message} className="mt-2" />
                     <PrimaryButton className="mt-4" disabled={processing}>Submit</PrimaryButton>
